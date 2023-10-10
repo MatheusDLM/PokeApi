8e3b33c2508fb1afba18070fa46141da3648b192
Amanda <amandamelquiades01@gmail.com>
2023-10-10 13:53:43 -0300

    feat: Pokemon listado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpService, Pokemon } from './http.service';
+import { Observable } from 'rxjs';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs';
+  pokemon!: Pokemon;
+  form: FormGroup; 
+  name = new FormControl('', Validators.required);
+  searchHistory: Pokemon[] = [];
+ 
+  constructor(private httpService: HttpService) {
+    this.form = new FormGroup({
+      name: this.name
+    });
+  }
+
+  onSubmit() {
+    const pokemonName = this.form.get('name')?.value;
+    console.log(pokemonName);
+    this.httpService.setPokemon(pokemonName);
+    this.getPokemon();
+  }
+
+  private getPokemon() {
+    this.httpService.getDados().subscribe((pokemon: Pokemon) => {
+      console.log(pokemon);
+      this.pokemon = pokemon;
+      this.addToSearchHistory(pokemon);
+    });
+  }
+
+  private addToSearchHistory(pokemon: Pokemon) {
+    this.searchHistory.unshift(pokemon);
+  }
+
+  removeFromHistory(index: number) {
+    this.searchHistory.splice(index, 1);
+  }
+  
 }
