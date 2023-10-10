d1309906cff9ef8672e374a4f6ebcee6fe65d21b
Nadson Santos <nadsonsouza02@gmail.com>
2023-10-10 17:03:45 -0300

    Feat: melhorias e exemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { HttpService, Pokemon } from './http.service';
-import { Observable } from 'rxjs';
+import { HttpService } from './http.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Pokemon } from './app.interface';
 
 @Component({
   selector: 'app-root',
@@ -11,26 +11,48 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent {
   title = 'rxjs';
   pokemon!: Pokemon;
-  form: FormGroup; 
-  name = new FormControl('', Validators.required);
+  form!: FormGroup<formGroup>; 
+
+  exampleFormControl: FormControl<boolean> = new FormControl(true, { validators: Validators.required, nonNullable: true });
+
+
+  cepControl: FormControl<string> = new FormControl('', { validators: Validators.required, nonNullable: true }); 
+  bairroControl: FormControl<string> = new FormControl('', { validators: Validators.required, nonNullable: true });
+  EstadoControl: FormControl<string> = new FormControl('', { validators: Validators.required, nonNullable: true });
+  CidadeControl: FormControl<string> = new FormControl('', { validators: Validators.required, nonNullable: true });
+
+  numeroControl: FormControl<number> = new FormControl(0, { validators: Validators.required, nonNullable: true });
+
+
   searchHistory: Pokemon[] = [];
- 
+
   constructor(private httpService: HttpService) {
-    this.form = new FormGroup({
-      name: this.name
-    });
+    this.createForm();
+    this.exampleFormControl.value;
+
+    const cep = this.cepControl.value;
+    const bairro = this.bairroControl.value;
+    const estado = this.EstadoControl.value;
+
+    this.form.value // { cep: valorCep, bairro: valorBairro, estado: valorEstado }
   }
 
   onSubmit() {
-    const pokemonName = this.form.get('name')?.value;
-    console.log(pokemonName);
-    this.httpService.setPokemon(pokemonName);
-    this.getPokemon();
+    const pokemonName = this.form.controls.name.value;
+
+
+
+    this.getPokemon(pokemonName);
   }
 
-  private getPokemon() {
-    this.httpService.getDados().subscribe((pokemon: Pokemon) => {
-      console.log(pokemon);
+  removeFromHistory(index: number) {
+    this.searchHistory.splice(index, 1);
+  }
+
+
+
+  private getPokemon(name: string) {
+    this.httpService.getDados(name).subscribe((pokemon: Pokemon) => {
       this.pokemon = pokemon;
       this.addToSearchHistory(pokemon);
     });
@@ -40,8 +62,22 @@ export class AppComponent {
     this.searchHistory.unshift(pokemon);
   }
 
-  removeFromHistory(index: number) {
-    this.searchHistory.splice(index, 1);
-  }
   
+  private createForm() {
+    this.form = new FormGroup<formGroup>({
+      name: new FormControl<string>('', { validators: Validators.required,  nonNullable: true }),
+
+      cep: new FormControl('', { validators: Validators.required, nonNullable: true }),
+      bairro: new FormControl('', { validators: Validators.required }),
+      numero: new FormControl<number>(0, { validators: Validators.required, nonNullable: true }),  
+    });
+  }
+
+}
+
+interface formGroup {
+  name: FormControl<string>,
+  cep: FormControl<string>,
+  numero: FormControl<number>,
+  bairro: FormControl<string | null>
 }
