6b8a9144ecb39e52854b38c48502761b03f13355
Nadson Santos <nadsonsouza02@gmail.com>
2023-10-11 10:22:09 -0300

    Feat: Inclusão Jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,16 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpService } from './http.service';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    const httpService = {} as HttpService;
+    component = new AppComponent(httpService);
   });
 
-  it(`should have as title 'rxjs'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('rxjs');
-  });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rxjs app is running!');
-  });
 });
--- src/app/http.service.spec.ts
+++ src/app/http.service.spec.ts
@@ -1,16 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HttpService } from './http.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('HttpService', () => {
   let service: HttpService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpService);
+    let _httpClient = {} as HttpClient;
+    service = new HttpService(_httpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
+  })
 });
