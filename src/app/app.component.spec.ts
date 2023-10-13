053b103a9d9e46925e492aaafd390fe0bd35b8b7
Amanda <amandamelquiades01@gmail.com>
2023-10-13 14:06:27 -0300

    testes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,79 @@
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpService } from './http.service';
+import { Pokemon } from './app.interface';
 
 describe('AppComponent', () => {
   let component: AppComponent;
 
   beforeEach(() => {
     const httpService = {} as HttpService;
-    component = new AppComponent(httpService);
+    const formBuilder = new FormBuilder();
+    component = new AppComponent(httpService, formBuilder);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   })
 
+
+  describe('method: onSubmit', () => {
+    it('should: vendo se esta enviando o formulario', () => {
+      (component as any).getPokemon = jest.fn();
+
+      const spyGetPokemon = jest.spyOn((component as any), 'getPokemon');
+
+      component.onSubmit();
+
+      expect(spyGetPokemon).toHaveBeenCalledWith('');
+    })
+  })
+
+  describe('method: addToSearchHistory', () => {
+    it('should: vendo se esta adicionando o card', () => {
+      component.searchHistory = [{} as Pokemon,{} as Pokemon];
+
+      (component as any).addToSearchHistory({} as Pokemon);
+
+      expect(component.searchHistory).toEqual([{} as Pokemon,{} as Pokemon,{} as Pokemon]);
+    })
+  })
+
+  describe('method: removeFromHistory', () => {
+    it('should: vendo se esta removendo o card', () => {
+      component.searchHistory = [{} as Pokemon,{} as Pokemon];
+
+      component.removeFromHistory(0);
+
+      expect(component.searchHistory).toEqual([{} as Pokemon]);
+    })
+  })
+
+
+  describe('method: createForm', () => {
+    // TODO? Exemplo teste valor da variavel
+    it('should: validar se formGroup existe no createForm', () => {
+      const spyGroup = jest.spyOn((component as any)._formBuilder, 'group');
+
+      (component as any).createForm();
+
+      expect(component.form.value).toEqual({
+        name:'',
+        cep:'',
+        bairro: '',
+        numero: 0
+      });
+
+      expect(spyGroup).toHaveBeenCalled();
+
+      // expect(component.form).toEqual(new FormGroup({
+      //   name: new FormControl<string>('', { validators: Validators.required,  nonNullable: true }),
+      //   cep: new FormControl('', { validators: Validators.required, nonNullable: true }),
+      //   bairro: new FormControl('', { validators: Validators.required }),
+      //   numero: new FormControl<number>(0, { validators: Validators.required, nonNullable: true }),
+      // }))
+    })
+  })
 });
-
-
-// TODO? Exemplo teste valor da variavel
-// expect(component.form).toEqual(new FormGroup({
-//   name: new FormControl<string>('', { validators: Validators.required,  nonNullable: true }),
-
-//   cep: new FormControl('', { validators: Validators.required, nonNullable: true }),
-//   bairro: new FormControl('', { validators: Validators.required }),
-//   numero: new FormControl<number>(0, { validators: Validators.required, nonNullable: true }),  
-// }))
 
 // expect(component.cepControl).toEqual(new FormControl('' , { validators: Validators.required, nonNullable: true }));
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpService } from './http.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Pokemon } from './app.interface';
 
 @Component({
@@ -11,30 +11,14 @@ import { Pokemon } from './app.interface';
 export class AppComponent {
   title = 'rxjs';
   pokemon!: Pokemon;
-  form!: FormGroup<formGroup>; 
-
-  exampleFormControl: FormControl<boolean> = new FormControl(true, { validators: Validators.required, nonNullable: true });
-
-
-  cepControl: FormControl<string> = new FormControl('', { validators: Validators.required, nonNullable: true }); 
-  bairroControl: FormControl<string> = new FormControl('', { validators: Validators.required, nonNullable: true });
-  EstadoControl: FormControl<string> = new FormControl('', { validators: Validators.required, nonNullable: true });
-  CidadeControl: FormControl<string> = new FormControl('', { validators: Validators.required, nonNullable: true });
-
-  numeroControl: FormControl<number> = new FormControl(0, { validators: Validators.required, nonNullable: true });
+  form!: FormGroup<formGroup>;
 
 
   searchHistory: Pokemon[] = [];
 
-  constructor(private httpService: HttpService) {
+  constructor(private httpService: HttpService, private _formBuilder: FormBuilder) {
     this.createForm();
-    this.exampleFormControl.value;
-
-    const cep = this.cepControl.value;
-    const bairro = this.bairroControl.value;
-    const estado = this.EstadoControl.value;
-
-    this.form.value // { cep: valorCep, bairro: valorBairro, estado: valorEstado }
+    // this.form.value // { cep: valorCep, bairro: valorBairro, estado: valorEstado }
   }
 
   onSubmit() {
@@ -47,8 +31,6 @@ export class AppComponent {
     this.searchHistory.splice(index, 1);
   }
 
-
-
   private getPokemon(name: string) {
     this.httpService.getDados(name)?.subscribe((pokemon: Pokemon) => {
       this.pokemon = pokemon;
@@ -60,16 +42,17 @@ export class AppComponent {
     this.searchHistory.unshift(pokemon);
   }
 
-  
+
   private createForm() {
-    this.form = new FormGroup<formGroup>({
+
+    //  this._formBuilder.group === new FormGroup()
+    this.form = this._formBuilder.group<formGroup>({
       name: new FormControl<string>('', { validators: Validators.required,  nonNullable: true }),
 
       cep: new FormControl('', { validators: Validators.required, nonNullable: true }),
       bairro: new FormControl('', { validators: Validators.required }),
-      numero: new FormControl<number>(0, { validators: Validators.required, nonNullable: true }),  
+      numero: new FormControl<number>(0, { validators: Validators.required, nonNullable: true }),
     });
-
 
     // this.cepControl = new FormControl(null, { validators: Validators.required });
   }
